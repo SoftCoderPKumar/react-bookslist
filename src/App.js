731b24389db1d11244b001f3b6250a608b454393
pappu kumar <pappu.setrosoft@gmail.com>
2018-12-27 11:47:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => (
+  <PersonList />
+)
+
+const PersonList = () => {
+  const people = [
+    {
+      img: "23",
+      name: "Pappu",
+      job: "Developer",
+      discription: "I am a sr Developer."
+    },
+    {
+      img: "36",
+      name: "Raju",
+      job: "Designer",
+      discription: "I am a Best Designer."
+    },
+    {
+      img: "25",
+      name: "Sumit",
+      job: "Developer",
+      discription: "I am a Jr Developer."
+    },
+    {
+      img: "38",
+      name: "rahman",
+      job: "Developer",
+      discription: "I am a Web designer."
+    },
+    {
+      img: "45",
+      name: "mukesh",
+      job: "Developer",
+      discription: "I am a Sr Developer."
+    },
+    {
+      img: "59",
+      name: "mubin",
+      job: "Developer",
+      discription: "I am a Jr Developer."
+    }
+  ];
+  return (
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]} />
+      <Person person={people[2]} >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eum quae molestias tempore minima in reiciendis sequi, cumque perferendis recusandae provident eos veritatis voluptates perspiciatis debitis explicabo ratione aliquam voluptatum.
+      </Person>
+      <Person person={people[3]} />
+      <Person person={people[4]} />
+      <Person person={people[5]} />
+    </section>
+  )
 }
+
+const Person = props => {
+  const { img, name, job, discription } = props.person;
+  const { children } = props;
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+
+  return <div className="person">
+    <img src={url} alt="" />
+    <div>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      <h5>{discription}</h5>
+      <p>{children}</p>
+
+    </div>
+  </div>
+}
+
+
 
 export default App;
